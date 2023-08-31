056d939c799c0e13f632d26f9704f32a2e52ea2d
rolando22 <rolando2207@gmail.com>
2023-08-31 18:23:06 -0300

    [UPDATE] HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Header } from './components';
 import { AppRoutes } from './routes/AppRoutes';
 import './App.css';
 
 export function App() {
     return (
-        <BrowserRouter basename='/mi-portfolio'>
+        <HashRouter>
             <Header />
             <AppRoutes />
-        </BrowserRouter>
+        </HashRouter>
     );
 }
