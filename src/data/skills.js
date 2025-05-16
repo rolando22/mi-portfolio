706fd6266bd86a40387e158ad9c134223f9b9831
rolando22 <rolando2207@gmail.com>
2025-05-16 16:55:08 -0300

    [UPDATE] Página de Habilidades

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,18 +1,13 @@
 export const skills = [
   { 
     id: crypto.randomUUID(), 
-    title: 'Paradigmas', 
-    technologies: ['Programación Funcional', 'Programación Orientada a Objetos'] 
-  },
-  { 
-    id: crypto.randomUUID(), 
     title: 'Lenguajes', 
     technologies: ['JavaScript', 'TypeScript'] 
   },
   { 
     id: crypto.randomUUID(), 
     title: 'Tecnologías para desarrollo Frontend', 
-    technologies: ['HTML5', 'CSS3', 'React', 'Redux', 'Zustand', 'Angular', 'Axios', 'SASS', 'Bootstrap', 'TailwindCSS'] 
+    technologies: ['HTML5', 'CSS3', 'React', 'React Router', 'Redux', 'Zustand', 'TanStack Query', 'Angular', 'NextJS', 'Axios', 'SASS', 'Bootstrap', 'TailwindCSS'] 
   },
   { 
     id: crypto.randomUUID(), 
@@ -21,8 +16,13 @@ export const skills = [
   },
   { 
     id: crypto.randomUUID(), 
-    title: 'Tecnologías Mobila', 
+    title: 'Tecnologías Mobile', 
     technologies: ['React Native', 'Expo'] 
+  },
+  { 
+    id: crypto.randomUUID(), 
+    title: 'Tecnologías Testing', 
+    technologies: ['Jest', 'Vitest', 'React Testing Library'] 
   },
   { 
     id: crypto.randomUUID(), 
@@ -31,7 +31,37 @@ export const skills = [
   }, 
   { 
     id: crypto.randomUUID(), 
+    title: 'Paradigmas', 
+    technologies: ['Programación Funcional', 'Programación Orientada a Objetos'] 
+  },
+  { 
+    id: crypto.randomUUID(), 
+    title: 'Patrones de Diseño', 
+    technologies: ['SOLID', 'Singleton', 'Factory', 'Repository'] 
+  },
+  { 
+    id: crypto.randomUUID(), 
+    title: 'Arquitectura', 
+    technologies: ['Clean Architecture'] 
+  },
+  { 
+    id: crypto.randomUUID(), 
     title: 'Infraestructura', 
     technologies: ['AWS', 'GitHub', 'GitLab', 'Firebase', 'Vercel', 'Docker'] 
+  }, 
+  { 
+    id: crypto.randomUUID(), 
+    title: 'Metodologías ágiles', 
+    technologies: ['SCRUM'] 
+  }, 
+  { 
+    id: crypto.randomUUID(), 
+    title: 'Gestores de proyectos', 
+    technologies: ['Jira', 'Trello'] 
+  }, 
+  { 
+    id: crypto.randomUUID(), 
+    title: 'Sistema de control de versiones', 
+    technologies: ['Git'] 
   }, 
 ];
